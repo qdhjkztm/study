e83e5a14b7113c31e233e56fc89bb278e54ce79b
qdhjkztm <qdhjkztm@hotmail.com>
2020-06-08 09:56:30 -0400

    增加通过指针交换值

--- c/hello.c
+++ c/hello.c
@@ -2,9 +2,11 @@
 void print(int i);
 int add(int a,int b);
 int getNumber();
+void changeValueViaPoint(int a,int b);
 int main(){
 	int m=getNumber();
 	int i=add(m,5);
 	print(i);
+	changeValueViaPoint(5,15);
 	return 0;
 }
