4edfbaea34de8c5b75f9cb1ae3122f8986daa77f
root <root@localhost.localdomain>
2020-06-08 22:33:25 -0400

    修改makefile

--- c/hello.c
+++ c/hello.c
@@ -3,10 +3,13 @@ void print(int i);
 int add(int a,int b);
 int getNumber();
 void changeValueViaPoint(int a,int b);
+void printArray(int array[]);
 int main(){
 	int m=getNumber();
 	int i=add(m,5);
 	print(i);
 	changeValueViaPoint(5,15);
+	int array[]={5,7,9};
+	printArray(array);
 	return 0;
 }
