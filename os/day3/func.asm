dba269b6aa47f44e18ef773e31a33227f16ad633
wangyipeng <wangyipeng@everjiankang.com>
2020-07-06 19:07:09 +0800

    修改文件

--- os/day3/func.asm
+++ os/day3/func.asm
@@ -5,17 +5,17 @@
 ; TAB=8
 [BITS 32]        ; 制作32位模式用的机械语言
 
-    GLOBAL    io_hlt    ; 程序中包含的函数名
-    GLOBAL    write_mem8
+	GLOBAL	io_hlt    ; 程序中包含的函数名
+	GLOBAL	write_mem8
 
 
 [SECTION .text]        
-io_hlt:    ; void io_hlt(void);
-    hlt
-    ret
+io_hlt:; void io_hlt(void);
+	hlt
+	ret
 
-write_mem8:    ; void write_mem*(int addr, int data);
-    mov    ecx,[esp+4]
-    mov    al,[esp+8]
-    mov    [ecx],al
-    ret
+write_mem8:	; void write_mem*(int addr, int data);
+	mov	ecx,[esp+4]
+	mov	al,[esp+8]
+	mov	[ecx],al
+	ret
