179e232d9f59e18667e8a112eaaeef8e3b4fae83
wangyipeng <wangyipeng@everjiankang.com>
2020-07-08 23:05:00 +0800

    add start

--- os/day3/start.c
+++ os/day3/start.c
@@ -1,6 +1,6 @@
 /* 告诉C编译器，有一个函数在别的文件里 */
 
-void io_hlt(void);
+void _start(void);
 
 /* 是函数声明却不用{}，而用;，这表示的意思是：
 	函数在别的文件中，你自己找一下 */
@@ -9,7 +9,7 @@ void main(void)
 {
 
 fin:
-	io_hlt(); /* 执行naskfunc.nas中的_io_hlt函数 */
+	_start(); /* 执行naskfunc.nas中的_io_hlt函数 */
 	goto fin;
 
 }
