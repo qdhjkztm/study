d7fbff1d296eec492a1096a3ee4d8696501845e5
wangyipeng <wangyipeng@everjiankang.com>
2020-07-11 20:55:26 +0800

    add lesson4

---
+++ os/day4/tools.asm
@@ -0,0 +1,18 @@
+printStr:
+	mov	bp, bx
+	call strlen
+strlen:
+	mov ax,bx			; ES:BP = 串地址
+calculate:
+	cmp	byte[eax],0
+	jz finished
+	inc ax
+	jmp calculate
+finished:
+	sub ax,bx
+	mov	cx, ax			; CX = 串长度
+	mov	ax, 01301h		; AH = 13,  AL = 01h
+	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	dl, 0
+	int	10h			; 10h 号中断
+	ret
