c5d93b58945b3328dd68390f1115caece33103c5
wangyipeng <wangyipeng@everjiankang.com>
2020-07-12 20:36:20 +0800

    forgot to add ,

--- huibian/lesson9/lesson9.asm
+++ huibian/lesson9/lesson9.asm
@@ -2,24 +2,26 @@
 section .data
 msg db 'please enter your name',0h
 msg2 db 'hello,',0h
+
 section .bbs
 sinput:	resb 255
+
 section .text
 global _start
 _start:
 	mov eax,msg
 	call println
 
-	mov edx 255
-	mov ecx sinput 
-	mov ebx 0
-	mov eax 3
+	mov edx,255
+	mov ecx,sinput 
+	mov ebx,0
+	mov eax,3
 	int 80h
 
 	mov eax,msg2
 	call println	
 
-	mov eax sinput
+	mov eax,sinput
 	call println
 
 	call quit
