b796058376a3c6461ce8561b28fc154b2608cf41
wangyipeng <wangyipeng@everjiankang.com>
2020-07-12 17:24:33 +0800

    edit func

--- os/day5/tools.asm
+++ os/day5/tools.asm
@@ -1,13 +1,21 @@
 printStr:
 	mov	bp, bx	; ES:BP = 串地址
+	push dx
+	push cx
+	push bx
+	push ax
 	call strlen
 
 	mov	cx, ax			; CX = 串长度
+	pop ax
 	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; 10h 号中断
-	
+
+	pop bx
+	pop cx
+	pop dx
 
 	ret
 strlen:
