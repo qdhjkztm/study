6a4d33e02af0ef83cfd3a26d4b6feaeb2bfe81a0
wangyipeng <wangyipeng@everjiankang.com>
2020-07-12 20:51:04 +0800

    edit bbs to bss

--- huibian/lesson9/lesson9.asm
+++ huibian/lesson9/lesson9.asm
@@ -1,27 +1,35 @@
-%include 'tools.asm'
-section .data
-msg db 'please enter your name:',0h
-msg2 db 'hello,',0h
+; Hello World Program (Getting input)
+; Compile with: nasm -f elf helloworld-input.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-input.o -o helloworld-input
+; Run with: ./helloworld-input
 
-section .bbs
-sinput:	resb 255
+%include 'tools.asm'
+
+section .data
+msg 		db 		'please enter your name:',0h
+msg2 		db 		'hello,',0h
+
+section .bss
+sinput:		resb 	255
 
 section .text
 global _start
+
 _start:
-	mov eax,msg
-	call print
 
-	mov edx,255
-	mov ecx,sinput 
-	mov ebx,0
-	mov eax,3
-	int 80h
+	mov 	eax,msg
+	call	print
 
-	mov eax,msg2
-	call print	
+	mov 	edx,255
+	mov 	ecx,sinput 
+	mov 	ebx,0
+	mov 	eax,3
+	int 	80h
 
-	mov eax,sinput
-	call print
+	mov 	eax,msg2
+	call 	print	
 
-	call quit
+	mov 	eax,sinput
+	call 	print
+
+	call 	quit
