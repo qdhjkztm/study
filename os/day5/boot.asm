2aee2d4025c4d92a9d7d73aa251eff7583091558
wangyipeng <wangyipeng@everjiankang.com>
2020-07-12 17:21:54 +0800

    add day5

---
+++ os/day5/boot.asm
@@ -0,0 +1,15 @@
+
+	org	07c00h			; 告诉编译器程序加载到7c00处
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	mov	bx, msg
+	call	printStr			; 调用显示字符串例程
+	mov	bx, msg2
+	call	printStr			; 调用显示字符串例程
+	jmp	$			; 无限循环
+%include 'tools.asm'
+msg:		db	"Hello, OS world! Second Good Start",0Ah,0h
+msg2:		db	"this is the second line",0Ah,0h
+times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
+dw 	0xaa55				; 结束标志
---
+++ os/day5/tools.asm
@@ -0,0 +1,25 @@
+printStr:
+	mov	bp, bx	; ES:BP = 串地址
+	call strlen
+
+	mov	cx, ax			; CX = 串长度
+	mov	ax, 01301h		; AH = 13,  AL = 01h
+	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	dl, 0
+	int	10h			; 10h 号中断
+	
+
+	ret
+strlen:
+	push bx
+	mov ax,bx			; ES:BP = 串地址
+calculate:
+	cmp	byte[eax],0
+	jz finished
+	inc ax
+	jmp calculate
+finished:
+	sub ax,bx
+	pop bx
+	ret
+	
