65886c3740c5609a56d4cde0a5701bca5cd9d7a3
wangyipeng <wangyipeng@everjiankang.com>
2020-07-16 23:10:42 +0800

    recover

--- os/day1/boot.asm
+++ os/day1/boot.asm
@@ -1,6 +1,5 @@
 
-	org	07c00h			; 告诉编译器程序加载到7c00处
-	mov	ax, cs
+	mov	ax, 07c00h
 	mov	ds, ax
 	mov	es, ax
 	call	DispStr			; 调用显示字符串例程
@@ -13,7 +12,7 @@ DispStr:
 	mov al, 0                             ; al为0,不移动光标，字符串中没有属性内容
 	mov bh, 0                             ; 第0页显示
 	mov bl, 0xFC                          ; 闪烁白色背景，红色加亮前景
-	mov	dl, 0
+	mov	dx, 0
 	int	10h			; 10h 号中断
 	ret
 msg:		db	"Hello, OS world!"
