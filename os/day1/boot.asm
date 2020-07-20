fb3cf757e74856697509f516996aca4452c1ab10
wangyipeng <wangyipeng@everjiankang.com>
2020-07-20 21:16:35 +0800

    lenth

--- os/day1/boot.asm
+++ os/day1/boot.asm
@@ -1,19 +1,19 @@
-	mov	ax, 07c00h
+	org	07c00h			; 告诉编译器程序加载到7c00处
+	mov	ax, cs
 	mov	ds, ax
 	mov	es, ax
 	call	DispStr			; 调用显示字符串例程
 	jmp	$			; 无限循环
 DispStr:
-	mov	ax, msg
+	mov	ax, BootMessage
 	mov	bp, ax			; ES:BP = 串地址
-	mov ah, 0x13                          ; ah为0x13,调用13号中断
-	mov	cx, 16			; CX = 串长度
-	mov al, 0                             ; al为0,不移动光标，字符串中没有属性内容
-	mov bh, 0                             ; 第0页显示
-	mov bl, 0xFC                          ; 闪烁白色背景，红色加亮前景
-	mov	dx, 0
+	mov	cx, len_string			; CX = 串长度
+	mov	ax, 01301h		; AH = 13,  AL = 01h
+	mov	bx, 000ch		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	dl, 0
 	int	10h			; 10h 号中断
 	ret
-msg:		db	"Hello, OS world!"
+BootMessage:		db	"Hello, OS world!"
+len_string  equ  $ - msg
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
-db 	0x55, 0xaa 				; 结束标志
+dw 	0xaa55				; 结束标志
