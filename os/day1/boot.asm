433f021a99d05e14330667ef869411e02dfc210e
wangyipeng <wangyipeng@everjiankang.com>
2020-08-01 22:35:42 +0800

    add day6

--- os/day1/boot.asm
+++ os/day1/boot.asm
@@ -8,7 +8,7 @@ DispStr:
 	mov	ax, BootMessage
 	mov	bp, ax			; ES:BP = 串地址
 	mov	cx, len_string			; CX = 串长度
-	mov	ax, 00e01h		; AH = 13,  AL = 01h
+	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ah		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; 10h 号中断
