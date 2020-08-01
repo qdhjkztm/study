bba83e03fbc7290a7e7b173177940a76a66b2644
wangyipeng <wangyipeng@everjiankang.com>
2020-08-01 23:04:04 +0800

    edit

--- huibian/lesson5/tools.asm
+++ huibian/lesson5/tools.asm
@@ -1,29 +1,29 @@
 strlen:
 	push ebx
-	mov  ebx,eax
+	mov  ebx,eax	;将msg存入ebx，开始通过eax自增计算msg的长度
 
 nextchar:
-	cmp  byte[eax],0
+	cmp  byte[eax],0	;这个以前的文章讲过，gdb调试那一章
 	jz   finished
-	inc  eax
+	inc  eax		
 	jmp  nextchar
 
 finished:
-	sub  eax,ebx
+	sub  eax,ebx	;计算长度后，将长度存在eax中
 	pop  ebx
 	ret
 
 print:
-	push edx
+	push edx	;push指令将edx压入栈中，需要用到edx原始值得时候，使用pop指令出栈即可，下面相同
 	push ecx
 	push ebx
 	push eax
 	call strlen
 	
-	mov edx,eax
-	pop eax
+	mov edx,eax	;
+	pop eax		;重新使eax内存地址指向的值为msg
 	
-	mov ecx,eax
+	mov ecx,eax	;固定将msg放入ecx
 	mov ebx,1
 	mov eax,4
 	int 80h
