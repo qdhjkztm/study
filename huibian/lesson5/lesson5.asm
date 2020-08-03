43bdf0240d8454e48f75a995d06c7dcbe3d0148a
wangyipeng <wangyipeng@everjiankang.com>
2020-08-03 22:42:15 +0800

    edit print out

--- huibian/lesson5/lesson5.asm
+++ huibian/lesson5/lesson5.asm
@@ -1,9 +1,9 @@
 %include 'tools.asm'
 section .data
-msg db 'hello world',0Ah
+msg db '123',0Ah
 section .text
 global _start
 _start:
-	mov eax,'0x1234'	;将msg放入eax寄存器，用来计算长度
+	mov eax,msg	;将msg放入eax寄存器，用来计算长度
 	call print	;调用print方法
 	call quit
