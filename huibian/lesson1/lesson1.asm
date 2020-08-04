bc71cc63316ff3f4536d8c34dbd3073ccd447e3e
wangyipeng <wangyipeng@everjiankang.com>
2020-08-04 22:58:06 +0800

    edit print

--- huibian/lesson1/lesson1.asm
+++ huibian/lesson1/lesson1.asm
@@ -7,9 +7,7 @@ section .text
 global main
 main:
     mov edx, len
-    mov ebx,edx
-    mov ds,ebx
-    mov ecx, [1234H]
+    mov ecx, [0]
     mov ebx, 1
     mov eax, 4 ;直接使用sys_write系统调用
     int 0x80
