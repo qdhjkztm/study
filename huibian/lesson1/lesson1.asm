087eaab98fd43b764d86eba4ac3dee418891c402
wangyipeng <wangyipeng@everjiankang.com>
2020-08-11 22:43:09 +0800

    edit print

--- huibian/lesson1/lesson1.asm
+++ huibian/lesson1/lesson1.asm
@@ -1,17 +1,17 @@
 section .data
 msg:
-    db "hello, world", 10
+    db 1, 10
 len equ $-msg
 
 section .text
 global main
 main:
-    mov rdx, len
-    mov rcx, 'hello'
-    mov rbx, 1
-    mov rax, 4 ;直接使用sys_write系统调用
+    mov edx, len
+    mov ecx,  msg
+    mov ebx, 1
+    mov eax, 4 ;直接使用sys_write系统调用
     int 0x80
 
-    mov rbx, 0
-    mov rax, 1
+    mov ebx, 0
+    mov eax, 1
     int 0x80
