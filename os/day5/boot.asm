1894542e9e92b9fdd8b5cf4b28538507cef1d11f
wangyipeng <wangyipeng@everjiankang.com>
2020-08-16 22:00:35 +0800

    edit print

--- os/day5/boot.asm
+++ os/day5/boot.asm
@@ -1,12 +1,11 @@
 
 	org	07c00h			; 告诉编译器程序加载到7c00处
-	mov	ax, cs
-	mov	ds, ax
-	mov	es, ax
+
 	mov	bx, msg
 	call	printStr			; 调用显示字符串例程
+
 	call	println
-	add bx,100
+
 	mov	bx, msg2
 	call	printStr			; 调用显示字符串例程
 	jmp	$			; 无限循环
