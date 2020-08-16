fc47ab372269d8f3c5fe2d0d03b6550d75fbc5ad
wangyipeng <wangyipeng@everjiankang.com>
2020-08-16 22:17:24 +0800

    edit print

--- os/day5/tools.asm
+++ os/day5/tools.asm
@@ -1,25 +1,22 @@
 printStr:
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
 	mov	bp, bx	; ES:BP = 串地址
-	push dx
-	push cx
-	push bx
-	push ax
+	pusha
 	call strlen
 
 	mov	cx, ax			; CX = 串长度
-	pop ax
+	
 	mov	ax, 01301h		; AH = 13,  AL = 01h
 	mov	bx, 000ah		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 2
 	int	10h			; 10h 号中断
 
-	pop bx
-	pop cx
-	pop dx
+	popa
 
 	ret
 strlen:
-	push bx
 	mov ax,bx			
 calculate:
 	cmp	byte[eax],0
@@ -28,7 +25,6 @@ calculate:
 	jmp calculate
 finished:
 	sub ax,bx
-	pop bx
 	ret
 
 println:
