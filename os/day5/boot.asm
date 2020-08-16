7b81df25a1989fb5529508e7a541277cd16d4665
wangyipeng <wangyipeng@everjiankang.com>
2020-08-16 21:56:22 +0800

    edit print

--- os/day5/boot.asm
+++ os/day5/boot.asm
@@ -11,7 +11,7 @@
 	call	printStr			; 调用显示字符串例程
 	jmp	$			; 无限循环
 %include 'tools.asm'
-msg:		db	"Hello, OS world! Second Good Start",0Ah,0h
-msg2:		db	"this is the second line",0Ah,0h
+msg:		db	"Hello, OS world! Second Good Start",0h
+msg2:		db	"this is the second line",0h
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw 	0xaa55				; 结束标志
