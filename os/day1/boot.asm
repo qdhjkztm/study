e1d6be4d8615405ba64550acb7cf4a98ec412931
wangyipeng <wangyipeng@everjiankang.com>
2020-08-16 22:38:00 +0800

    edit print

--- os/day1/boot.asm
+++ os/day1/boot.asm
@@ -12,8 +12,18 @@ DispStr:
 	mov	bx, 000ah		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
 	mov	dl, 0
 	int	10h			; 10h 号中断
+
+	mov	ax, BootMessage2
+	mov	bp, ax			; ES:BP = 串地址
+	mov	cx, len_string2			; CX = 串长度
+	mov	ax, 01301h		; AH = 13,  AL = 01h
+	mov	bx, 000ah		; 页号为0(BH = 0) 黑底红字(BL = 0Ch,高亮)
+	mov	dl, 0
+	int	10h			; 10h 号中断
 	ret
 BootMessage:		db	"Hello, OS world!"
+BootMessage2:		db	"aaa"
 len_string  equ  $ - BootMessage
+len_string2  equ  $ - BootMessage2
 times 	510-($-$$)	db	0	; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw 	0xaa55				; 结束标志
