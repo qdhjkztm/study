4922c4d50ae6fe0ede96359a541959d46a093050
wangyipeng <wangyipeng@everjiankang.com>
2020-08-17 22:56:39 +0800

    add day10

--- os/day10/print.asm
+++ os/day10/print.asm
@@ -8,7 +8,7 @@ print:
 start:
     mov al, [bx] ; 'bx' is the base address for the string
     cmp al, 0 
-    je done
+    jz done
 
     ; the part where we print with the BIOS help
     mov ah, 0x0e
