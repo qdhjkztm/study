8b751b55ad0580eb7511703339c547f253519953
wangyipeng <wangyipeng@everjiankang.com>
2020-08-18 22:14:33 +0800

    add

---
+++ huibian/lesson11/lesson10.asm
@@ -0,0 +1,30 @@
+; Hello World Program (Getting input)
+; Compile with: nasm -f elf helloworld-input.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-input.o -o helloworld-input
+; Run with: ./helloworld-input
+
+%include 'tools.asm'
+
+section .data
+; data
+HELLO:
+    db 'Hello, World', 0
+
+GOODBYE:
+    db 'Goodbye', 0
+
+section .text
+global main
+
+main:
+
+	mov bx, HELLO
+	call print
+
+	call print_nl
+
+	mov bx, GOODBYE
+	call print
+
+	call print_nl
+
