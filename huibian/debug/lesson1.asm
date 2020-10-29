58eb4031c089d9e4c89c1f303ce1b036421237be
qdhjkztm <qdhjkztm@hotmail.com>
2020-10-29 19:30:18 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -1,18 +1,11 @@
 section .data
-msg:
-    db "hello,world", 10
-len equ $-msg
+
 
 section .text
 global main
 main:
-    mov edx, len
-    mov word [0x601024],  'h'
-    mov ecx,[0x601024]
-    mov ebx, 1
-    mov eax, 4 ;直接使用sys_write系统调用
-    int 0x80
+    move    ax,1000
+    move    bx,ax
+    add     bx,ax
+    add     ax,bx
 
-    mov ebx, 0
-    mov eax, 1
-    int 0x80
