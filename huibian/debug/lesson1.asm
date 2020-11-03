661bfbdd2b0678bf02aa4f204c94c61afdadb92c
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-03 18:55:07 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -2,11 +2,10 @@ section .data
 section .text
 global main
 main:
-    mov     eax,'aaa'
-    mov     ebx,eax
-    add     ebx,eax
-    add     eax,ebx
-    push    rax
-    mov     eax,[0]
-    inc     eax
-    add     eax,eax
+    mov     ecx,'aaa'
+    push    ecx
+    mov     ecx,esp
+    mov     edx,3
+    mov     eax,4
+    mov     ebx,1
+    int     80h
