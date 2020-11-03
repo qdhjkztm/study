db6c49475964e785156760e6fd89052ad16231c6
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-03 19:06:14 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -9,3 +9,7 @@ main:
     mov     eax,4
     mov     ebx,1
     int     80h
+
+    mov     ebx, 0      ; return 0 status on exit - 'No Errors'
+    mov     eax, 1      ; invoke SYS_EXIT (kernel opcode 1)
+    int     80h
