a1827e054fccfa6aad44006b97a0f46ca0ff6d59
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-04 20:23:51 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -2,7 +2,8 @@ section .data
 section .text
 global main
 main:
-    mov     rax, "aaa"    ; move 0Ah into rax - 0Ah is the ascii character for a linefeed
+    mov     rax, 1
+    add     rax,48
     push    rax         ; push the linefeed onto the stack so we can get the address
     mov     rax, rsp    ; move the address of the current stack pointer into rax for sprint
     call    sprint
