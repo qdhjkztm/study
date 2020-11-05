d7ba94b9bdd1f0896fb782b08f912ef3a6ebe6b2
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-05 20:32:08 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -1,69 +1,8 @@
+%include    'function.asm'     
 section .data
 section .text
 global main
  
-main:
-    mov     eax, 0100h        ; move the address of our integer into eax
-    push    eax             ; push eax to the stack
-    mov     eax, esp        ; get the address of the character on the stack
-    call    sprintLF        ; call our print function
- 
-    pop     eax             ; clean up the stack so we don't have unneeded bytes taking up space
-    
- 
-    call    quit
-quit:
-    mov     ebx, 0
-    mov     eax, 1
-    int     80h
-    ret
-slen:
-    push    ebx
-    mov     ebx, eax
- 
-nextchar:
-    cmp     byte [eax], 0
-    jz      finished
-    inc     eax
-    jmp     nextchar
- 
-finished:
-    sub     eax, ebx
-    pop     ebx
-    ret
- 
- 
-;------------------------------------------
-; void sprint(String message)
-; String printing function
-sprint:
-    push    edx
-    push    ecx
-    push    ebx
-    push    eax
-    call    slen
- 
-    mov     edx, eax
-    pop     eax
- 
-    mov     ecx, eax
-    mov     ebx, 1
-    mov     eax, 4
-    int     80h
- 
-    pop     ebx
-    pop     ecx
-    pop     edx
-    ret
-
-sprintLF:
-    call    sprint
- 
-    push    eax         ; push eax onto the stack to preserve it while we use the eax register in this function
-    mov     eax, 0Ah    ; move 0Ah into eax - 0Ah is the ascii character for a linefeed
-    push    eax         ; push the linefeed onto the stack so we can get the address
-    mov     eax, esp    ; move the address of the current stack pointer into eax for sprint
-    call    sprint      ; call our sprint function
-    pop     eax         ; remove our linefeed character from the stack
-    pop     eax         ; restore the original value of eax before our function was called
-    ret                 ; return to our program
+main:        ; move the address of our integer into eax
+    mov     eax,'111'    ; return to our program
+    call    printSrt
