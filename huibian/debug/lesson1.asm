f08dff6061c939d911d2236b93db7d4110018722
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-05 11:32:20 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -4,40 +4,40 @@ global _start
  
 _start:
  
-    mov     rcx, 0          ; rcx is initalised to zero.
+    mov     ecx, 0          ; ecx is initalised to zero.
  
 nextNumber:
-    inc     rcx             ; increment rcx
+    inc     ecx             ; increment ecx
  
-    mov     rax, rcx        ; move the address of our integer into rax
-    add     rax, 48         ; add 48 to our number to convert from integer to ascii for printing
-    push    rax             ; push rax to the stack
-    mov     rax, rsp        ; get the address of the character on the stack
+    mov     eax, ecx        ; move the address of our integer into eax
+    add     eax, 48         ; add 48 to our number to convert from integer to ascii for printing
+    push    eax             ; push eax to the stack
+    mov     eax, esp        ; get the address of the character on the stack
     call    sprintLF        ; call our print function
  
-    pop     rax             ; clean up the stack so we don't have unneeded bytes taking up space
-    cmp     rcx, 10         ; have we reached 10 yet? compare our counter with decimal 10
+    pop     eax             ; clean up the stack so we don't have unneeded bytes taking up space
+    cmp     ecx, 10         ; have we reached 10 yet? compare our counter with decimal 10
     jne     nextNumber      ; jump if not equal and keep counting
  
     call    quit
 quit:
-    mov     rbx, 0
-    mov     rax, 1
+    mov     ebx, 0
+    mov     eax, 1
     int     80h
     ret
 slen:
-    push    rbx
-    mov     rbx, rax
+    push    ebx
+    mov     ebx, eax
  
 nextchar:
-    cmp     byte [rax], 0
+    cmp     byte [eax], 0
     jz      finished
-    inc     rax
+    inc     eax
     jmp     nextchar
  
 finished:
-    sub     rax, rbx
-    pop     rbx
+    sub     eax, ebx
+    pop     ebx
     ret
  
  
@@ -45,33 +45,33 @@ finished:
 ; void sprint(String message)
 ; String printing function
 sprint:
-    push    rdx
-    push    rcx
-    push    rbx
-    push    rax
+    push    edx
+    push    ecx
+    push    ebx
+    push    eax
     call    slen
  
-    mov     rdx, rax
-    pop     rax
+    mov     edx, eax
+    pop     eax
  
-    mov     rcx, rax
-    mov     rbx, 1
-    mov     rax, 4
+    mov     ecx, eax
+    mov     ebx, 1
+    mov     eax, 4
     int     80h
  
-    pop     rbx
-    pop     rcx
-    pop     rdx
+    pop     ebx
+    pop     ecx
+    pop     edx
     ret
 
 sprintLF:
     call    sprint
  
-    push    rax         ; push rax onto the stack to preserve it while we use the rax register in this function
-    mov     rax, 0Ah    ; move 0Ah into rax - 0Ah is the ascii character for a linefeed
-    push    rax         ; push the linefeed onto the stack so we can get the address
-    mov     rax, rsp    ; move the address of the current stack pointer into rax for sprint
+    push    eax         ; push eax onto the stack to preserve it while we use the eax register in this function
+    mov     eax, 0Ah    ; move 0Ah into eax - 0Ah is the ascii character for a linefeed
+    push    eax         ; push the linefeed onto the stack so we can get the address
+    mov     eax, esp    ; move the address of the current stack pointer into eax for sprint
     call    sprint      ; call our sprint function
-    pop     rax         ; remove our linefeed character from the stack
-    pop     rax         ; restore the original value of rax before our function was called
+    pop     eax         ; remove our linefeed character from the stack
+    pop     eax         ; restore the original value of eax before our function was called
     ret                 ; return to our program
