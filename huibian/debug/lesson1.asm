632171c14d21c96c2b92be5b30d82131a0cc9e98
qdhjkztm <qdhjkztm@hotmail.com>
2020-11-06 19:01:30 +0800

    test

--- huibian/debug/lesson1.asm
+++ huibian/debug/lesson1.asm
@@ -6,10 +6,11 @@ global main
 main:        ; move the address of our integer into eax
     mov     eax,'111'    ; return to our program
     call    printStr
-    
+
     mov     eax,100
     mov     ebx,1000
-    add     eax,ebx
+    sub     ebx,eax
+    mov     eax,ebx
     call    printInteger
 
     call    quit
